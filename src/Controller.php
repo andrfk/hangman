4e0c8d8b4c73c170b60aef4b6c5e056bf8f8ba64
Andrey Kokulov <74734040+andrfk@users.noreply.github.com>
2020-11-20 10:15:40 +0300

    Add files via upload

--- src/Controller.php
+++ src/Controller.php
@@ -1,8 +1,58 @@
-<?php namespace andrfk\hangman\Controller;
-    use function andrfk\hangman\View\showGame;
+<?php 
 
-    function startGame() {
-        echo "The game started".PHP_EOL;
-        showGame();
+namespace andrfk\hangman\Controller;
+use function andrfk\hangman\View\showGame;
+
+function showResult ($answers, $word)
+{
+    
+    if ($answers == 4) {
+        echo "\n You won!";
     }
+
+    else {
+        echo "\n You lose!";
+    }
+
+    echo "\n The hidden word was: $word\n";
+}
+
+function startGame() 
+{
+    $word = "answer";
+    
+    $remaining = substr($word, 1, -1);
+
+    $entryField = "______";
+    $entryField[0] = $word[0];
+    $entryField[-1] = $word[-1];
+
+    $fails = 0;
+    $answers = 0;
+
+    do {
+        showGame($fails, $entryField);
+        $letter = mb_strtolower(readline("Letter: "));
+        $attempt = 0;
+
+        for ($i = 0; $i < strlen($remaining); $i++) {
+            if ($remaining[$i] == $letter ) {
+                $entryField[$i + 1] = $letter;
+                $remaining[$i] = " ";
+                $answers++;
+                $attempt++;
+            }
+        }
+
+        if ($attempt == 0) {
+            $fails++;
+        }
+
+
+            } while ($fails != 6 && $answers != 4);
+
+            showGame($fails, $entryField);
+            showResult($answers, $word);        
+}
+
 ?>
--- src/View.php
+++ src/View.php
@@ -1,6 +1,28 @@
 <?php namespace andrfk\hangman\View;
 
-    function showGame() {
-        echo "The game displayed";
+    function showGame($fails, $entryField) {
+        $pseudographics = array (
+            " +---+\n     |\n     |\n     |\n    ===\n ",
+		    " +---+\n 0   |\n     |\n     |\n    ===\n ",
+		    " +---+\n 0   |\n |   |\n     |\n    ===\n ",
+		    " +---+\n 0   |\n/|   |\n     |\n    ===\n ",
+		    " +---+\n 0   |\n/|\  |\n     |\n    ===\n ",
+		    " +---+\n 0   |\n/|\  |\n/    |\n    ===\n ",
+		    " +---+\n 0   |\n/|\  |\n/ \  |\n    ===\n "
+        );
+
+        echo $pseudographics[$fails];
+
+        
+
+        for ($i = 0; $i < strlen($entryField); $i++) {
+            echo $entryField[$i];
+        }
+
+        echo "\n";
+
+        echo "\n";
     }
+
+    
 ?>
