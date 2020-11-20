b11c13537fb8177b1c752750a19edfec631bc4bc
Andrey Kokulov <74734040+andrfk@users.noreply.github.com>
2020-11-20 23:51:06 +0300

    Add files via upload

--- src/Controller.php
+++ src/Controller.php
@@ -1,58 +1,134 @@
-<?php 
+<?php
 
 namespace andrfk\hangman\Controller;
-use function andrfk\hangman\View\showGame;
 
-function showResult ($answers, $word)
+use function cli\line;
+use function cli\prompt;
+use function andrfk\hangman\Model\dbOpen;
+use function andrfk\hangman\View\showGame;
+use function andrfk\hangman\Model\dbUpdate;
+use function andrfk\hangman\Model\gameList;
+use function andrfk\hangman\Model\gameReplay;
+
+function gameMenu()
 {
-    
-    if ($answers == 4) {
-        echo "\n You won!";
+    $userName = prompt("Enter your name");
+
+    if (!empty($userName)) {
+        while (true) {
+            $key = prompt(
+                "\nEnter key:\n"
+                . "--new - new game\n"
+                . "--list - games list\n"
+                . "--replay id - replay game with current \n"
+                . "--exit - exit\n"
+            );
+
+            if ($key === '--new') {
+                startGame($userName);
+            } elseif ($key === '--list') {
+                gameList();
+            } elseif (preg_match('/(^--replay [\d]+$)/', $key) !== 0) {
+                gameReplay(explode(' ', $key)[1]);
+            } elseif ($key === '--exit') {
+                exit();
+            } else {
+                line("Wrong key\n");
+            }
+        }
+    } else {
+        gameMenu();
     }
 
-    else {
+
+}
+
+function showResult($answers, $word)
+{
+    if ($answers === 4) {
+        echo "\n You won!";
+    } else {
         echo "\n You lose!";
     }
 
     echo "\n The hidden word was: $word\n";
 }
 
-function startGame() 
+function startGame($playerName)
 {
-    $word = "answer";
-    
-    $remaining = substr($word, 1, -1);
+    $action = dbOpen();
 
-    $entryField = "______";
-    $entryField[0] = $word[0];
-    $entryField[-1] = $word[-1];
+    $words = array("answer", "abroad", "animal", "action");
+    $word = $words[array_rand($words)];
+
+    date_default_timezone_set("Europe/Moscow");
+    $gameDate = date("d") . "." . date("m") . "." . date("Y");
+    $gameTime = date("H") . ":" . date("i") . ":" . date("s");
+
+    $action->exec(
+        "INSERT INTO gamesInfo (
+        gameDate,
+        gameTime,
+        playerName,
+        word,
+        result) VALUES (
+            '$gameDate',
+            '$gameTime',
+            '$playerName',
+            '$word',
+            'Игра не закончена')"
+    );
+
+    $gameId = $action->querySingle("SELECT gameId FROM gamesInfo ORDER BY gameId DESC LIMIT 1");
+
+    $remaining = substr($word, 1, -1);
+    $maxAnswers = strlen($remaining);
+    $maxFails = 6;
+    $gameField = "______";
+    $gameField[0] = $word[0];
+    $gameField[-1] = $word[-1];
 
     $fails = 0;
     $answers = 0;
+    $attempts = 0;
 
     do {
-        showGame($fails, $entryField);
-        $letter = mb_strtolower(readline("Letter: "));
-        $attempt = 0;
+        showGame($fails, $gameField);
+        $letter = mb_strtolower(prompt("Letter: "));
+        $attemptCount = 0;
 
-        for ($i = 0; $i < strlen($remaining); $i++) {
-            if ($remaining[$i] == $letter ) {
-                $entryField[$i + 1] = $letter;
+        for ($i = 0, $iMax = strlen($remaining); $i < $iMax; $i++) {
+            if ($remaining[$i] === $letter) {
+                $gameField[$i + 1] = $letter;
                 $remaining[$i] = " ";
                 $answers++;
-                $attempt++;
+                $attemptCount++;
             }
         }
 
-        if ($attempt == 0) {
+        if ($attemptCount === 0) {
             $fails++;
+            $result = 'lose';
+        } else {
+            $result = 'win';
         }
 
+        $attempts++;
 
-            } while ($fails != 6 && $answers != 4);
+        $action->exec(
+            "INSERT INTO attemptsInfo (
+            gameId,
+            attempts,
+            letter,
+            result) VALUES (
+                '$gameId',
+                '$attempts',
+                '$letter',
+                '$result')"
+        );
+    } while ($fails < $maxFails && $answers < $maxAnswers);
 
-            showGame($fails, $entryField);
-            showResult($answers, $word);        
+    showGame($fails, $gameField);
+    showResult($answers, $word);
+    dbUpdate($gameId, $result);
 }
-
-?>
